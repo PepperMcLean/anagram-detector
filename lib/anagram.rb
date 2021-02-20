01ba54d3933be0a640a398365318a61460d95474
PepperMcLean <jackmcl206@gmail.com>
2021-02-20 12:59:34 -0600

    commit tree c68fd27945ad2b0d1e010af8bd25f52bfeb704c8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+
+    def match(array)
+        arrayTwo = []
+        comparison = @word.chars.sort.join
+        array.each do |potentialWord|
+            x = potentialWord.chars.sort.join
+            if x == comparison
+                arrayTwo << potentialWord
+            end
+        end
+        return arrayTwo
+    end
+
+
+end
